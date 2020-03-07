fe4babccaf661a0fca4e5d64e74f2c8e78e31d19
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-03-07 20:05:13 +0700

    Add the code of insertion sort

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+function insertionSort(numbers) {
+	
+	for (var i = 1; i < numbers.length; i++) {
+		
+		for (var j = i; j > 0; j--) {
+			if (numbers[j] < numbers[j - 1]) {
+				var temp = numbers[j];
+				numbers[j] = numbers[j - 1];
+				numbers[j - 1] = temp;
+			}
+			else
+				continue;
+		}
+	}
+}
 
+var numbers = [5, 8, 2, 4, 9, 3, 10, 1, 4];
+
+insertionSort(numbers);
+
+console.log(numbers);
